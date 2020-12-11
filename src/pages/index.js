25b12edb15b01df640a92ff715ae146295ec93a7
danjarosz <jarosz.daniel@protonmail.com>
2020-12-11 20:08:53 +0100

    feat(index.js): set the new content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello.</h1>
+      <h2>
+        I'm Daniel, a front-end developer living in beautiful Miechowice, Poland.
+      </h2>
+    </div>
+  )
 }
+
+export default IndexPage;
