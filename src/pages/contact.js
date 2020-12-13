94b5808fcba8710cb8c8a0dcf89e4deac9761a9d
danjarosz <jarosz.daniel@protonmail.com>
2020-12-13 11:41:39 +0100

    feta(Layout): implement

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Layout from "../components/layout";
 
 const ContactPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Contact</h1>
       <p>
-        The best way to reach me is on <a href="https://www.linkedin.com/in/daniel-jarosz-b83456151/" target="_blank">LinkedIn</a> 
+        The best way to reach me is on <a href="https://www.linkedin.com/in/daniel-jarosz-b83456151/" target="blank">LinkedIn</a> 
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+    <Layout>
       <h1>Hello.</h1>
       <h2>
         I'm Daniel, a front-end developer living in beautiful Miechowice, Poland.
@@ -14,8 +12,7 @@ const IndexPage = () => {
       <p>
         Need a developer? <Link to="/contact">Contact me.</Link>
       </p>
-      <Footer />
-    </div>
+    </Layout>
   )
 };
 
