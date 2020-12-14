54ba3a7521aa8017865136c7f0d6653da4d4bebc
danjarosz <jarosz.daniel@protonmail.com>
2020-12-14 19:41:47 +0100

    feat: add content of posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from "../components/layout";
 
- const Blog = () => {
+
+// slug variable comes from pageContext
+export const query = graphql`
+  query (
+    $slug: String
+  ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+ const Blog = (props) => {
+  // data comes from the query from above and it is given with props 
+  const { data } = props;
+
+  const { markdownRemark: { 
+    frontmatter: { title, date },
+    html
+  } } = data
+
   return (
     <Layout>
-      This is the blog template.
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
     </Layout>
   )
  }
