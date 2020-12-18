47c86715b2321b5499d7d884e4f25404391cef59
danjarosz <jarosz.daniel@protonmail.com>
2020-12-18 18:01:43 +0100

    feat: create dynamic paths from CMD data slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,51 @@
 const path = require('path');
 
-// creating new fields to setup dynamic routes for blog posts
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
+//-------------------------------------
+// // It is used to create dynamic path from markdown (.md)
+// // creating new fields to setup dynamic routes for blog posts
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions;
 
-  if(node.internal.type === 'MarkdownRemark') {
-    const slug = path.basename(node.fileAbsolutePath, '.md')
-    createNodeField({
-      node,
-      name: 'slug',
-      value: slug
-    })
-  }
-}
+//   if(node.internal.type === 'MarkdownRemark') {
+//     const slug = path.basename(node.fileAbsolutePath, '.md')
+//     createNodeField({
+//       node,
+//       name: 'slug',
+//       value: slug
+//     })
+//   }
+// }
+//-------------------------------------
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
   //1. Get path to template
   const blogTemplate = path.resolve('./src/templates/blog.js');
-
   //2. Get markdown data
+  //-------------------------------------
+  // //2a creating pages of markdown (.md)
+  // const res = await graphql(`
+  //   query {
+  //     allMarkdownRemark {
+  //       edges {
+  //         node {
+  //           fields {
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+  //-------------------------------------
+  // //2b creating pages from CMS (Contentful)
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
@@ -36,12 +53,26 @@ module.exports.createPages = async ({ graphql, actions }) => {
   `);
 
   //3. Get new pages
-  res.data.allMarkdownRemark.edges.forEach((edge) => {
+  //-------------------------------------
+  // 3a Form markdown
+  // res.data.allMarkdownRemark.edges.forEach((edge) => {
+  //   createPage({
+  //     component: blogTemplate,
+  //     path: `/blog/${edge.node.fields.slug}`,
+  //     context: {
+  //       slug: edge.node.fields.slug
+  //     }
+  //   })
+  // })
+  //-------------------------------------
+  //3b from CMS (Contentful)
+  res.data.allContentfulBlogPost.edges.forEach((edge) => {
+    console.log(edge)
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug
+        slug: edge.node.slug
       }
     })
   })
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -10,42 +10,50 @@ import Layout from "../components/layout";
 
 // Searchnig the posts given as markdown
 // slug variable comes from pageContext
-export const query = graphql`
-  query (
-    $slug: String
-  ) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
-        }
-      }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
-    }
-  }
-`
+// export const query = graphql`
+//   query (
+//     $slug: String
+//   ) {
+//     markdownRemark (
+//       fields: {
+//         slug: {
+//           eq: $slug
+//         }
+//       }
+//     ) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 
  const Blog = (props) => {
   // data comes from the query from above and it is given with props 
   const { data } = props;
 
-  const { markdownRemark: { 
-    frontmatter: { title, date },
-    html
-  } } = data
 
   return (
     <Layout>
-      <h1>{title}</h1>
-      <p>{date}</p>
-      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+
     </Layout>
   )
+
+  // // Solution for markdown
+  // const { markdownRemark: { 
+  //   frontmatter: { title, date },
+  //   html
+  // } } = data
+
+  // return (
+  //   <Layout>
+  //     <h1>{title}</h1>
+  //     <p>{date}</p>
+  //     <div dangerouslySetInnerHTML={{ __html: html }}></div>
+  //   </Layout>
+  // )
  }
 
  export default Blog;
